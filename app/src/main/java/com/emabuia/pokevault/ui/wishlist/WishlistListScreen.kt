be02ac62f7049020ebf26a704e008e0ba7c52abe
PokeVault <emanuelebuia@live.it>
2026-04-16 22:37:29 +0200

    - Fixed Build APK

--- app/src/main/java/com/emabuia/pokevault/ui/wishlist/WishlistListScreen.kt
+++ app/src/main/java/com/emabuia/pokevault/ui/wishlist/WishlistListScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -319,6 +320,7 @@ private fun WishlistRow(
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun CreateWishlistDialog(
     onDismiss: () -> Unit,
