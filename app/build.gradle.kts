119b083405c8d4281e1ff042d5af654da9b66de4
PokeVault <emanuelebuia@live.it>
2026-04-16 13:54:27 +0200

    - Fix EdgeToEdge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ android {
 dependencies {
     // ── Core Android ──
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
 
