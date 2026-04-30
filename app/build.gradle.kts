8cc8a99641655a4aff2b92bebeedb2890ca4a754
PokeVault <emanuelebuia@live.it>
2026-04-30 23:37:04 +0200

    - Fix 16KB and collapse slow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.emabuia.pokevault"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "2.0.10"
+        versionCode = 19
+        versionName = "2.0.11"
 
         buildConfigField("String", "POKETCG_API_KEY", "\"${localProperties.getProperty("POKETCG_API_KEY", "")}\"")
         buildConfigField("String", "POKEWALLET_API_KEY", "\"${localProperties.getProperty("POKEWALLET_API_KEY", "")}\"")
