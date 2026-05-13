b1f6f75d267309fe378e42fd31faade1db5ac5c2
PokeVault <emanuelebuia@live.it>
2026-05-13 23:19:15 +0200

    - Update Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.emabuia.pokevault"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "2.0.16"
+        versionCode = 25
+        versionName = "2.0.17"
 
         buildConfigField("String", "POKETCG_API_KEY", "\"${localProperties.getProperty("POKETCG_API_KEY", "")}\"")
         buildConfigField("String", "POKEWALLET_API_KEY", "\"${localProperties.getProperty("POKEWALLET_API_KEY", "")}\"")
